0785da0b5ee23a1b557bb876e912e8dca117cea3
LucasSBento <profissional.lucasbento@gmail.com>
2026-03-11 10:54:44 -0300

    Tentando implementar o novo input system

---
+++ Aprendizado Jogos/Assets/Player/TestingInputSystem.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public float jumpForce;
+    public float vel;
+    public Rigidbody2D rb;
+    public Vector2 vector;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        vector = context.ReadValue<Vector2>();
+    }
+
+    public void FixedUpdate()
+    {
+        rb.linearVelocity = vel * vector * Time.deltaTime;
+    }
+}
